a4c9774b1b0d0a25e981d3287e06f25bbe49f3d8
Natasha <natasha.pinder00@gmail.com>
2021-08-05 14:10:09 +0100

    added some styling and cols

--- src/Weather.js
+++ src/Weather.js
@@ -5,17 +5,29 @@ import "./Weather.css";
 export default function Weather() {
     return (  
         <div className="Weather">
+            <form>
+             <input type="search"
+             placeholder="Enter a city..." 
+             className="Form-control" />
+
+<input type="submit" value="Search" className="btn btn-primary"/>
+
+            </form>
+
 <h1>London</h1>
 <ul>
 <li> Thursday 13:00</li>
 <li>Mostly cloudy</li>
 </ul>
+
 <div className="row">
+
     <div className="col-6">
         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostyl cloudy"/>
         21°C°F
-    </div>
-    <div className="col-6">
+   </div>
+     
+    <div className="col-3">
         <ul>
             <li>
                 Precipitation: 12%
@@ -24,16 +36,19 @@ export default function Weather() {
 Humidity: 59%
  </li>
  <li>  
-Wind: 9 mph
+Wind: 9 mp/h
  </li>
 
            
         </ul>
     </div>
-</div>
+     </div>
+      </div>
+ 
 
+   
+   
 
-
-        </div>
+       
     )
 }
