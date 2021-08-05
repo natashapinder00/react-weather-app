0482f1c83e7fbbe71679b03860f2454083bb9659
Natasha <natasha.pinder00@gmail.com>
2021-08-05 13:52:42 +0100

    added weather css

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,39 @@
 import React from "react";
+import "./Weather.css";
 
 
 export default function Weather() {
     return (  
-        <div>Hello from weather</div>
+        <div className="Weather">
+<h1>London</h1>
+<ul>
+<li> Thursday 13:00</li>
+<li>Mostly cloudy</li>
+</ul>
+<div className="row">
+    <div className="col-6">
+        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostyl cloudy"/>
+        21°C°F
+    </div>
+    <div className="col-6">
+        <ul>
+            <li>
+                Precipitation: 12%
+                </li>
+              <li>  
+Humidity: 59%
+ </li>
+ <li>  
+Wind: 9 mph
+ </li>
+
+           
+        </ul>
+    </div>
+</div>
+
+
+
+        </div>
     )
 }
