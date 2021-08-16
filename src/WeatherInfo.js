10ded15bb0f38f5a04c2a6cae54644ce3feb36d7
Natasha <natasha.pinder00@gmail.com>
2021-08-16 14:54:01 +0100

    updated the icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,17 +11,23 @@ export default function WeatherInfo(props) {
 <li>
     <FormattedDate date={props.data.date}/>
 </li>
-<li className="text-capitalize">{props.data.description} </li>
+<li className="text-capitalize">{props.data.description} 
+</li>
 </ul>
 
 <div className="row mt-3">
     <div className="col-6">
         <div className="clearfix">
-        <div className="float-left">
-         <WeatherIcon code={props.data.icon} />
-         </div>
+            <div className="float-left">
+
+           
+            <WeatherIcon
+             code={props.data.icon}  />
+             </div>
+       
       
   <div className="float-left">
+      
       <WeatherTemperature celsius={props.data.temperature} />
       
        </div>
